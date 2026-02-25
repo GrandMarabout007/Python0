035f26863ba5f28a0210bfd14a0005a8ccc91083
robin <robin.schimmer@hotmail.fr>
2026-02-25 18:11:25 +0100

    pute

--- Python05/ex2/nexus_pipeline.py
+++ Python05/ex2/nexus_pipeline.py
@@ -1,7 +1,81 @@
-from typing import Any, List, Dict, Union, Optional
+from typing import Any, List, Dict, Union, Optional, Protocol
 from abc import ABC, abstractmethod
+
+
+
+class ProcessingStage(Protocol):
+    def process(self, data: Any) -> Any:
+
+
+
+
+
+
+class ProcessingPipeline(ABC):
+    def __init__(self, pipeline_id: str):
+        self.pipeline_id = pipeline_id
+
+    @abstractmethod
+    def process(self, data: Any) -> Any:
+        pass
+
+    def add_stage():
+
+class InputStage():
+    def process(self, data: Any) -> dict:
+        return f"Input validation and parsing {data}"
+
+class TransformStage():
+    def process(self, data: Any) -> dict:
+        return f"Data transformation and enrichment {data}"
+
+class OutputStage():
+    def process(self, data: Any) -> str:
+        return f"Output formatting and delivery{data}"
+    
+
+
+class JSONAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        super().__init__(pipeline_id)
+
+    def process(self, data: Any) -> Union[str, Any]:
+        return 
+
+class CSVAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        super().__init__(pipeline_id)
+
+    def process(self, data: Any) -> Union[str, Any]:
+        return 
+
+class StreamAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        super().__init__(pipeline_id)
+
+    def process(self, data: Any) -> Union[str, Any]:
+        return
+
+
+
+
+class NexusManager:
+    """polymorph multiple pipeline
+    """
+    @staticmethod
+    def process_data(streams: list):
+        for stream in streams:
+            stream.process()
+    
+    def add_pipeline():
+
+
+def nexus_pipeline():
+    json_data = {"sensor": "temp", "value": 23.5, "unit": "C"}
+    csv_data = "user, action,timestamp"
+    stream_data = "Real-time sensor stream"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 
 
 if __name__ == "__main__":
     print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===\n")
-    code_nexus()
+    nexus_pipeline()
